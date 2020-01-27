67dab013aa0bca17cce87551f89b8ce397a213c3
jasfuncs <56724011+jasfuncs@users.noreply.github.com>
2020-01-27 17:28:18 +0200

    Check for flush hands

--- holdem-odds/Hand.cs
+++ holdem-odds/Hand.cs
@@ -6,13 +6,88 @@ namespace holdem_odds
 {
 	class Hand
 	{
+		enum Type
+		{
+			None,
+			HighCard,
+			OnePair,
+			TwoPair,
+			ThreeOfAKind,
+			Straight,
+			Flush,
+			FullHouse,
+			FourOfAKind,
+			StraightFlush
+		}
+
 		public Card holeCard1 = null;
 		public Card holeCard2 = null;
 
-		public Card communityCards = null;
-		public Card communityCard2 = null;
-		public Card communityCard3 = null;
-		public Card communityCard4 = null;
-		public Card communityCard5 = null;
+		public bool GetBestHand(List<Card> communityCards)
+		{
+			if (GetFlushCards(communityCards) != null)
+			{
+				Console.WriteLine("Flush");
+				return true;
+			}
+			return false;
+		}
+
+		// If the player has a flush, returns the flushed cards. Otherwise, returns null.
+		private List<Card> GetFlushCards(List<Card> communityCards)
+		{
+			List<Card> flushCards = new List<Card>();
+
+			flushCards = GetSuitedCards(communityCards, Card.Suit.Clubs);
+			if (flushCards.Count >= 5)
+			{
+				return flushCards;
+			}
+
+			flushCards = GetSuitedCards(communityCards, Card.Suit.Diamonds);
+			if (flushCards.Count >= 5)
+			{
+				return flushCards;
+			}
+
+			flushCards = GetSuitedCards(communityCards, Card.Suit.Hearts);
+			if (flushCards.Count >= 5)
+			{
+				return flushCards;
+			}
+
+			flushCards = GetSuitedCards(communityCards, Card.Suit.Spades);
+			if (flushCards.Count >= 5)
+			{
+				return flushCards;
+			}
+
+			return null;
+		}
+
+		private List<Card> GetSuitedCards(List<Card> communityCards, Card.Suit suit)
+		{
+			List<Card> suitedCards = new List<Card>();
+
+			if (holeCard1.suit == suit)
+			{
+				suitedCards.Add(holeCard1);
+			}
+
+			if (holeCard2.suit == suit)
+			{
+				suitedCards.Add(holeCard2);
+			}
+
+			for (int i = 0; i < communityCards.Count; i++)
+			{
+				if (communityCards[i].suit == suit)
+				{
+					suitedCards.Add(communityCards[i]);
+				}
+			}
+
+			return suitedCards;
+		}
 	}
 }
--- holdem-odds/Program.cs
+++ holdem-odds/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace holdem_odds
 {
@@ -6,7 +7,34 @@ namespace holdem_odds
 	{
 		static void Main(string[] args)
 		{
-			
+			while (true)
+			{
+				//Console.Clear();
+
+				Deck deck = new Deck();
+				deck.Shuffle();
+
+				Hand hand = new Hand();
+				hand.holeCard1 = deck.DrawNextCard();
+				hand.holeCard2 = deck.DrawNextCard();
+
+				Console.WriteLine("Hole cards: " + hand.holeCard1.GetHumanReadable() + " " + hand.holeCard2.GetHumanReadable());
+				Console.Write("Table: ");
+
+				List<Card> communityCards = new List<Card>();
+				for (int i = 0; i < 5; i++)
+				{
+					var nextCard = deck.DrawNextCard();
+					communityCards.Add(nextCard);
+					Console.Write(nextCard.GetHumanReadable() + " ");
+				}
+
+				if (hand.GetBestHand(communityCards))
+				{
+					Console.Read();
+				}
+				Console.WriteLine("\n");
+			}
 		}
 	}
 }

d9cb355fd85835f6103e95477a115f6d5db469c1
jasfuncs <56724011+jasfuncs@users.noreply.github.com>
2020-01-27 15:25:34 +0200

    Create Hand.cs

---
+++ holdem-odds/Hand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace holdem_odds
+{
+	class Hand
+	{
+		public Card card1 = new Card();
+		public Card card2 = new Card();
+	}
+}

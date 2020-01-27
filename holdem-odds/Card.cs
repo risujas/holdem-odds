1896167a51ee87a8a3f7eadc161631bafdfed3ed
jasfuncs <56724011+jasfuncs@users.noreply.github.com>
2020-01-27 15:23:37 +0200

    Add Card.cs, add HandEvaluator.cs

---
+++ holdem-odds/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace holdem_odds
+{
+	class Card
+	{
+		public enum Suit
+		{
+			Clubs,
+			Diamonds,
+			Hearts,
+			Spades
+		}
+
+		public enum Value
+		{
+			V2 = 2,
+			V3 = 3,
+			V4 = 4,
+			V5 = 5,
+			V6 = 6,
+			V7 = 7,
+			V8 = 8,
+			V9 = 9,
+			VT = 10,
+			VJ = 11,
+			VQ = 12,
+			VK = 13,
+			VA = 14
+		}
+
+		public Suit suit;
+		public Value value;
+	}
+}
--- holdem-odds/Program.cs
+++ holdem-odds/Program.cs
@@ -6,7 +6,7 @@ namespace holdem_odds
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
 		}
 	}
 }

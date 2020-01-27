962faabb53560d9b4936317b5bb2cefdf3ca954d
jasfuncs <56724011+jasfuncs@users.noreply.github.com>
2020-01-27 16:54:44 +0200

    Rename HoleCards as Hand, include community cards

---
+++ holdem-odds/Hand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace holdem_odds
+{
+	class Hand
+	{
+		public Card holeCard1 = null;
+		public Card holeCard2 = null;
+
+		public Card communityCards = null;
+		public Card communityCard2 = null;
+		public Card communityCard3 = null;
+		public Card communityCard4 = null;
+		public Card communityCard5 = null;
+	}
+}
